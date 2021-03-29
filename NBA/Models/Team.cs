f9e25c6fb11f2458957f602b540541047b8a0b2a
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-29 11:10:10 -0700

    create team class
    Co-authored-by: Tien Nguyen<tien96ng@gmail.com>

---
+++ NBA/Models/Team.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NBA.Models
+{
+  public class Team
+  {
+    public Team()
+    {
+      this.Players = new HashSet<Player>();
+    }
+
+    public int TeamId { get; set; }
+
+    [Required]
+    [StringLength(60)]
+    public string TeamName { get; set; }
+    [Required]
+    
+    public string Location { get; set; }
+    [Range(0, 255, ErrorMessage = "Number of Nba Championships should be between 0 and 255.")]
+    public int NbaChampionships { get; set; }
+    public virtual ICollection<Player> Players { get; set; }
+
+    }
+}
